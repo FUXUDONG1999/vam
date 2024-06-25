c783ffbcf6e36b4e267b2a73edc579efdb019c0f
xudongffu <xudongffu@tencent.com>
2024-06-25 19:46:32 +0800

    运行程序

--- src/main/java/com/xudong/vam/runner/GameRunner.java
+++ src/main/java/com/xudong/vam/runner/GameRunner.java
@@ -2,6 +2,8 @@ package com.xudong.vam.runner;
 
 import com.xudong.vam.runner.enums.GameMode;
 
+import java.io.IOException;
+
 public interface GameRunner {
-    void run(GameMode gameMode);
+    void run(GameMode gameMode) throws IOException;
 }
